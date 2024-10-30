7d79035990e63779f6150233a7f1c1d705e9a801
giabao1112002 <148312025+giabao1112002@users.noreply.github.com>
2024-10-30 19:04:59 +0700

    final

--- App.tsx
+++ App.tsx
@@ -7,36 +7,68 @@
 
 import React, {useState} from 'react';
 import Product from './Products';
+import Add from './Add';
+import ProductSearch from './Product_Search';
+import ProductDetail from './Product_Detail';
+
+import {SafeAreaView} from 'react-native-safe-area-context';
+
 import {
-  SafeAreaView,
-  FlatList,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  BottomNavigation,
+  MD3LightTheme,
+  PaperProvider,
+} from 'react-native-paper';
 
-const App = () => {
-  const [data, setData] = useState();
-  const filePath = 'https://dummyjson.com/products';
+export default function App() {
+  const [index, setIndex] = useState(0);
+
+  const [routes] = useState([
+    {key: 'ProductList', title: 'Products', focusedIcon: 'folder'},
+    {key: 'Product_Add', title: 'Add', focusedIcon: 'folder'},
+    {key: 'ProductSearch', title: 'Search', focusedIcon: 'find'},
+    {key: 'Product_Detail', title: 'ProductDetail', focusedIcon: 'calendar'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    ProductList: Product,
+    Product_Add: Add,
+    ProductSearch: ProductSearch,
+    Product_Detail: ProductDetail,
+  });
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Product List</Text>
-    </SafeAreaView>
+    <PaperProvider theme={MD3LightTheme}>
+      <SafeAreaView style={{flex: 1}}>
+        <BottomNavigation
+          navigationState={{index, routes}}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </SafeAreaView>
+    </PaperProvider>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 40,
-    textAlign: 'center',
-    color: 'black',
-  },
-  container: {
-    marginLeft: 10,
-    marginRight: 10,
-  },
-});
+// const App = () => {
+//   const [data, setData] = useState();
+//   const filePath = 'https://dummyjson.com/products';
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.title}>Product List</Text>
+//     </SafeAreaView>
+//   );
+// };
 
-export default Product;
+// const styles = StyleSheet.create({
+//   title: {
+//     fontSize: 40,
+//     textAlign: 'center',
+//     color: 'black',
+//   },
+//   container: {
+//     marginLeft: 10,
+//     marginRight: 10,
+//   },
+// });
+
+// export default Product;
